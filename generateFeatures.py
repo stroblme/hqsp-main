bb2aa5dd006e799f4eed337f72158f1d6e6ca704
stroblme <melvin.strobl@web.de>
2021-07-08 16:51:25 +0200

    main_qsr import

--- generateFeatures.py
+++ generateFeatures.py
@@ -2,10 +2,11 @@ from stqft.frontend import frontend, signal, transform
 from stqft.stqft import stqft_framework
 from stqft.stft import stft_framework
 
-from qcnn import main_qsr
+from qcnn.main_qsr import gen_train_from_wave, labels
 
 frontend.enableInteractive()
 
 windowLength = 2**10
 overlapFactor=0.5
 windowType='hann'
+
