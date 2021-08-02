13178babc68bd5386a7474b55fc45c0af3caa505
stroblme <melvin.strobl@web.de>
2021-08-02 09:06:09 +0200

    basic loading and feature gen

--- generateFeatures.py
+++ generateFeatures.py
@@ -10,14 +10,22 @@ from stqft.stft import stft_framework
 
 # from qcnn.main_qsr import gen_train_from_wave, labels
 
+def genFeature(speechFile):
+    y = signal(samplingRate=16000, signalType='file', path=speechFile)
+    stqft = transform(stqft_framework, suppressPrint=True, minRotation=0.2)
+    y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType)
+    y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel', fmax=4000)
+
 
 windowLength = 2**10
 overlapFactor=0.5
 windowType='hann'
 
-datasetPath = "./dataset"
+datasetPath = "/ceph/mstrobl/dataset"
 
 datasetFiles = glob.glob(datasetPath + "/**/*.wav", recursive=True)
 
 print(f"Found {len(datasetFiles)} files in the dataset")
 
+for dataFile in datasetFiles:
+    genFeature(dataFile)
