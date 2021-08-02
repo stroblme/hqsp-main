7e67e5ae982732c23a9ea62fe426547c332c5532
stroblme <melvin.strobl@web.de>
2021-08-02 08:59:25 +0200

    dataset listing

--- generateFeatures.py
+++ generateFeatures.py
@@ -1,12 +1,23 @@
+import sys
+sys.path.append("./stqft")
+sys.path.append("./qcnn")
+
+import glob
+
 from stqft.frontend import frontend, signal, transform
 from stqft.stqft import stqft_framework
 from stqft.stft import stft_framework
 
-from qcnn.main_qsr import gen_train_from_wave, labels
+# from qcnn.main_qsr import gen_train_from_wave, labels
 
-frontend.enableInteractive()
 
 windowLength = 2**10
 overlapFactor=0.5
 windowType='hann'
+
+datasetPath = "./dataset"
+
+datasetFiles = glob.glob(datasetPath + "/**/*.wav", recursive=True)
+
+print(f"Found {len(datasetFiles)} files in the dataset")
 
