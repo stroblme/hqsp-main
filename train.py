38499db94f41bbd5844b57b909ba0e55a4768847
stroblme <melvin.strobl@web.de>
2021-08-02 14:38:32 +0200

    initial train

---
+++ train.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append("./stqft")
+sys.path.append("./qcnn")
+
+import os
+#Activate the cuda env
+os.environ["LD_LIBRARY_PATH"] = "$LD_LIBRARY_PATH:/usr/local/cuda/lib64/:/usr/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda-11.2/lib64:/usr/local/cuda/targets/x86_64-linux/lib/"
+
+import glob
+import numpy as np
+import time
+
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.utils import plot_model
+
+from stqft.frontend import frontend, signal, transform
+from stqft.stqft import stqft_framework
+from stqft.stft import stft_framework
+
+from qcnn.main_qsr import gen_train_from_wave, labels
+from qcnn.models import attrnn_Model
+
+datasetPath = "/ceph/mstrobl/dataset"
+outputPath = "/ceph/mstrobl/features/"
+
+batchSize = 16
+epochs = 30
+
+x_train = np.load(outputPath + "x_train_speech.npy")
+x_valid = np.load(outputPath + "x_test_speech.npy")
+y_train = np.load(outputPath + "y_train_speech.npy")
+y_valid = np.load(outputPath + "y_test_speech.npy")
+
+
+q_train = x_train
+q_valid = x_valid
+
+## For Quanv Exp.
+early_stop = EarlyStopping(monitor='val_loss', mode='min', 
+                        verbose=1, patience=10, min_delta=0.0001)
+
+metric = 'val_accuracy'
+
+checkpoint = ModelCheckpoint('checkpoints/best_speech.hdf5', monitor=metric, 
+                            verbose=1, save_best_only=True, mode='max')
+
+
+model = attrnn_Model(q_train[0], labels)
+
+model.summary()
+plot_model(model, to_file='model.png')
+
+history = model.fit(
+    x=q_train, 
+    y=y_train,
+    epochs=epochs, 
+    callbacks=[checkpoint], 
+    batch_size=batchSize, 
+    validation_data=(q_valid,y_valid)
+)
+
+data_ix = time.strftime("%Y%m%d_%H%M")
+
+model.save('checkpoints/'+ data_ix + '_speech.hdf5')
