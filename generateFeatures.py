d4eefefcd9b4cbe6b71b22bbea69eab6957e7b2e
stroblme <melvin.strobl@web.de>
2021-08-09 17:07:08 +0200

    checkpoints

--- generateFeatures.py
+++ generateFeatures.py
@@ -9,6 +9,8 @@ os.environ["LD_LIBRARY_PATH"] = "$LD_LIBRARY_PATH:/usr/local/cuda/lib64/:/usr/li
 import glob
 import numpy as np
 
+import time
+import pickle
 
 from stqft.frontend import frontend, signal, transform
 from stqft.stqft import stqft_framework
@@ -16,19 +18,27 @@ from stqft.stft import stft_framework
 
 from qcnn.main_qsr import gen_train_from_wave, labels
 
+
 windowLength = 2**10
 overlapFactor=0.5
 windowType='hann'
 
 datasetPath = "/ceph/mstrobl/dataset"
-outputPath = "/ceph/mstrobl/features/"
+waveformPath = "/ceph/mstrobl/waveforms"
+featurePath = "/ceph/mstrobl/features/"
+
+av = 0
 
 def gen_mel(speechFile, sr=16000):
+    start = time.time()
+
     y = signal(samplingRate=sr, signalType='file', path=speechFile)
     stqft = transform(stqft_framework, suppressPrint=True, minRotation=0.2, numOfShots=1024)
     y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType, suppressPrint=True)
     y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel', fmax=4000)
 
+    diff = time.time()-start
+    print(f"Iteration took {diff} s")
     return y_hat_stqft_p
 
 def gen_train(labels, train_audio_path, outputPath, sr=16000, port=1):
@@ -51,7 +61,14 @@ def gen_train(labels, train_audio_path, outputPath, sr=16000, port=1):
             all_wave.append(np.expand_dims(wave, axis=2))
             all_label.append(label)
 
-    print(f"Finished generating waveforms. Start exporting features")
+    print(f"Finished generating waveforms at {time.time()}")
+    
+    with open(f"{waveformPath}/waveforms{time.time()}.pckl", 'wb') as fid:
+        pickle.dump(all_wave, fid, pickle.HIGHEST_PROTOCOL)
+    with open(f"{waveformPath}/labels{time.time()}.pckl", 'wb') as fid:
+        pickle.dump(all_label, fid, pickle.HIGHEST_PROTOCOL)
+        
+    print(f"Finished dumping cache. Starting Feature export")
 
     return gen_train_from_wave(all_wave=all_wave, all_label=all_label, output=outputPath)
 
@@ -59,4 +76,4 @@ datasetFiles = glob.glob(datasetPath + "/**/*.wav", recursive=True)
 
 print(f"Found {len(datasetFiles)} files in the dataset")
 
-gen_train(labels, datasetPath, outputPath, port=2)
+gen_train(labels, datasetPath, featurePath, port=2)
