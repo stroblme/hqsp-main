2a7850fb4480de0b006babe5b2debf06cd1f9a5b
stroblme <melvin.strobl@web.de>
2021-08-10 14:03:50 +0200

    cleanup and model predict

--- test.py
+++ test.py
@@ -14,9 +14,10 @@ import time
 
 import tensorflow as tf
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.utils import plot_model
+from tensorflow.keras.utils import plot_model, to_categorical
 from tensorflow.keras.models import load_model
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
+from sklearn.preprocessing import LabelEncoder
 
 from stqft.frontend import frontend, signal, transform
 from stqft.stqft import stqft_framework
@@ -24,20 +25,22 @@ from stqft.stft import stft_framework
 
 from qcnn.main_qsr import labels
 
-windowLength = 2**10
-overlapFactor=0.5
+windowLength = 2**10    #1024
+overlapFactor=0.125     #128
 windowType='hann'
 
 datasetPath = "/ceph/mstrobl/testDataset"
-checkpoints = "/ceph/mstrobl/checkpoints/"
+modelsPath = "/ceph/mstrobl/models"
 
-model = load_model_from_hdf5(f"{checkpoints}/data_ix_speech.hdf5")
+models = sorted(glob.glob(f"{modelsPath}/**"), key = os.path.getmtime)
+
+model = load_model(models[0], compile = True)
 
 def gen_mel(speechFile, sr=16000):
     start = time.time()
 
     y = signal(samplingRate=sr, signalType='file', path=speechFile)
-    stqft = transform(stqft_framework, suppressPrint=True, minRotation=0.2, numOfShots=1024)
+    stqft = transform(stqft_framework, suppressPrint=False, minRotation=0.2, numOfShots=1024)
     y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=windowLength, overlapFactor=overlapFactor, windowType=windowType, suppressPrint=True)
     y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel', fmax=4000)
 
@@ -45,31 +48,32 @@ def gen_mel(speechFile, sr=16000):
     print(f"Iteration took {diff} s")
     return y_hat_stqft_p
 
-def do_test(labels, train_audio_path, sr=16000, port=1):
+def do_test(labels, train_audio_path, sr=16000):
     for label in labels:
         datasetLabelFiles = glob.glob(f"{train_audio_path}/{label}/*.wav")
 
         all_wave = list()
         all_label = list()
 
-        portDatsetLabelFiles = datasetLabelFiles[0::port]
-        print(f"Using {len(portDatsetLabelFiles)} out of {len(datasetLabelFiles)} files for label '{label}'")
-
-        it = 0
-        for datasetLabelFile in portDatsetLabelFiles:
-            print(f"Processing '{datasetLabelFile}' in label '{label}' [{it}/{len(portDatsetLabelFiles)}]")
+        it = 1
+        for datasetLabelFile in datasetLabelFiles:
+            print(f"Processing '{datasetLabelFile}' in label '{label}' [{it}/{len(datasetLabelFiles)}]")
             it+=1
 
-            wave = gen_mel(datasetLabelFile, sr)
+            try:
+                wave = gen_mel(datasetLabelFile, sr)
+            except Exception as e:
+                print(f"Error: {e}")
+                continue
 
             all_wave.append(np.expand_dims(wave, axis=2))
             all_label.append(label)
 
-            y_pred = np.argmax(model.predict(all_wave[-1]), axis=1)
-            print(f"Model returned {y_pred} and label was {label} in file {datasetLabelFile}")
+        y_pred = np.argmax(model.predict(all_wave), axis=1)
+        print(f"\nModel returned {labels[y_pred[0]]} and label was {label} in file {datasetLabelFile}\n")
 
 datasetFiles = glob.glob(datasetPath + "/**/*.wav", recursive=True)
 
 print(f"Found {len(datasetFiles)} files in the dataset")
 
-do_test(labels, datasetPath, port=2)
+do_test(labels, datasetPath)
