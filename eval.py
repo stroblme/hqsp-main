b8884a00bb65bf7a5aed467d8cb4bc5703f2cf10
stroblme <melvin.strobl@web.de>
2021-08-12 15:44:35 +0200

    updated according to latest changes in frontend

--- eval.py
+++ eval.py
@@ -1,4 +1,6 @@
 import sys
+
+from numpy import fmax
 sys.path.append("./stqft")
 sys.path.append("./qcnn")
 
@@ -18,14 +20,15 @@ y = signal(samplingRate=sr, signalType='file', path=speechFile)
 
 stqft = transform(stqft_framework, minRotation=0.2, numOfShots=2048, suppressPrint=True, fixZeroSignal=True)
 y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=1024, overlapFactor=0.875, windowType='hann')
-y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='None', fmax=8000, normalize=False)
+y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel', normalize=False, samplingRate=y.samplingRate, nMels=60, fmin=40.0, fmax=y.samplingRate/2)
+# y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='none', normalize=False)
 
-mel_basis = librosa.filters.mel(sr, f.size, n_mels=60, fmin=40.0, fmax=sr/2)
+# mel_basis = librosa.filters.mel(sr, f.size, n_mels=60, fmin=40.0, fmax=sr/2)
 
-y_hat_stqft_p_mel = np.dot(mel_basis[:,1:], y_hat_stqft_p)
+# y_hat_stqft_p_mel = np.dot(mel_basis[:,1:], y_hat_stqft_p)
 
 
 # test_plot(y_rosa_hat, sr)
-test_plot(y_hat_stqft_p_mel, sr)
+test_plot(y_hat_stqft_p, sr)
 
 input()
