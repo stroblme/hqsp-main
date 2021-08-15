5a351e85c65de6479f73cc140f953e50b14d22b4
stroblme <melvin.strobl@web.de>
2021-08-15 11:13:10 +0200

    cleanup

--- generateQuanvFeatures.py
+++ generateQuanvFeatures.py
@@ -6,21 +6,9 @@ import os
 #Activate the cuda env
 os.environ["LD_LIBRARY_PATH"] = "$LD_LIBRARY_PATH:/usr/local/cuda/lib64/:/usr/lib64:/usr/local/cuda/extras/CUPTI/lib64:/usr/local/cuda-11.2/lib64:/usr/local/cuda/targets/x86_64-linux/lib/"
 print(os.environ.get("LD_LIBRARY_PATH"))
-import glob
 import numpy as np
-import time
 
-import tensorflow as tf
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
-from tensorflow.keras.utils import plot_model
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
-
-from stqft.frontend import frontend, signal, transform
-from stqft.stqft import stqft_framework
-from stqft.stft import stft_framework
-
-from qcnn.main_qsr import gen_train_from_wave, labels, gen_quanv
-from qcnn.models import attrnn_Model
+from qcnn.small_qsr import gen_quanv
 
 datasetPath = "/ceph/mstrobl/dataset"
 featurePath = "/ceph/mstrobl/features"
@@ -37,5 +25,5 @@ y_train = np.load(f"{featurePath}/y_train_speech.npy")
 y_valid = np.load(f"{featurePath}/y_test_speech.npy")
 
 
-q_train, q_valid = gen_quanv(x_train, x_valid, 2, saveTo=quantumPath) 
+q_train, q_valid = gen_quanv(x_train, x_valid, 2, output=quantumPath) 
 
