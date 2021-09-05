ff4d37302448b9e164b8e1fa58524e3c90ad4f8e
stroblme <melvin.strobl@web.de>
2021-09-05 11:21:52 +0200

    switched to hamming

--- generateFeatures.py
+++ generateFeatures.py
@@ -37,7 +37,7 @@ def gen_mel(speechFile):
 
     y = signal(samplingRate=sr, signalType='file', path=speechFile)
     stqft = transform(stqft_framework, numOfShots=2048, suppressPrint=True, signalFilter=True, minRotation=0.1)
-    y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=1024, overlapFactor=0.875, windowType='blackman', suppressPrint=True)
+    y_hat_stqft, f, t = stqft.forward(y, nSamplesWindow=1024, overlapFactor=0.875, windowType='hamming', suppressPrint=True)
     y_hat_stqft_p, f_p, t_p = stqft.postProcess(y_hat_stqft, f ,t, scale='mel', normalize=True, samplingRate=y.samplingRate, nMels=60, fmin=40.0, fmax=y.samplingRate/2)
 
     diff = time.time()-start
