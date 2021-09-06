5e566fe0d5d5593097b2f2404ed0f6a2bc5c042a
stroblme <melvin.strobl@web.de>
2021-09-06 09:43:38 +0200

    added export mechanism

--- train.py
+++ train.py
@@ -22,11 +22,14 @@ quantumPath = "/ceph/mstrobl/data_quantum"
 waveformPath = "/ceph/mstrobl/waveforms"
 checkpointsPath = "/ceph/mstrobl/checkpoints"
 
-TOPIC = "training"
+exportPath = "/ceph/mstrobl/versioning"
+
+TOPIC = "PrepGenTrain"
 
 samplingRate = 16000
 batchSize = 16
 epochs = 30
+port = 10
 PoolSize = int(multiprocessing.cpu_count()*0.3) #be gentle..
 
 if __name__ == '__main__':
@@ -52,7 +55,7 @@ if __name__ == '__main__':
 
     print(f"Found {len(datasetFiles)} files in the dataset")
 
-    exp = export(topic=TOPIC, identifier="dataset")
+    exp = export(topic=TOPIC, identifier="dataset", dataDir=exportPath)
     exp.setData(export.DESCRIPTION, f"Dataset {len(datasetFiles)} in {datasetPath}")
     exp.doExport()
 
@@ -63,13 +66,18 @@ if __name__ == '__main__':
     from qcnn.small_qsr import labels
     
     if args.waveform:
-        x_train, x_valid, y_train, y_valid = gen_features(labels, datasetPath, featurePath, PoolSize, waveformPath=waveformPath, port=10, samplingRate=samplingRate)
+        x_train, x_valid, y_train, y_valid = gen_features(labels, datasetPath, featurePath, PoolSize, waveformPath=waveformPath, port=port, samplingRate=samplingRate)
     else:
         print("Loading from disk...")
         x_train = np.load(f"{featurePath}/x_train_speech.npy")
         x_valid = np.load(f"{featurePath}/x_test_speech.npy")
         y_train = np.load(f"{featurePath}/y_train_speech.npy")
         y_valid = np.load(f"{featurePath}/y_test_speech.npy")
+
+    exp = export(topic=TOPIC, identifier="waveforms", dataDir=exportPath)
+    exp.setData(export.DESCRIPTION, f"Waveforms generated (T)/ loaded (F): {args.waveform}; Labels used: {labels}; FeaturePath: {featurePath}; PoolSize: {PoolSize}; WaveformPath: {waveformPath}; Portioning: {port}, SamplingRate: {samplingRate}")
+    exp.setData(export.GENERICDATA, {"x_train":x_train, "x_valid":x_valid, "y_train":y_train, "y_valid":y_valid})
+    exp.doExport()
 
     print(f"\n\n\n-----------------------\n\n\n")
     print(f"Generating Quantum Data @{time.time()}")
@@ -81,6 +89,11 @@ if __name__ == '__main__':
         print("Loading from disk...")
         q_train = np.load(f"{quantumPath}/quanv_train.npy")
         q_valid = np.load(f"{quantumPath}/quanv_test.npy")
+
+    exp = export(topic=TOPIC, identifier="quantumData", dataDir=exportPath)
+    exp.setData(export.DESCRIPTION, f"Quantum data generated (T)/ loaded (F): {args.quantum}; FeaturePath: {quantumPath}")
+    exp.setData(export.GENERICDATA, {"q_train":q_train, "q_valid":q_valid})
+    exp.doExport()
 
     print(f"\n\n\n-----------------------\n\n\n")
     print(f"Starting Training @{time.time()}")
@@ -95,3 +108,8 @@ if __name__ == '__main__':
         model.save(f"{modelsPath}/model_{time.time()}")
     else:
         print("Training disabled")
+
+    exp = export(topic=TOPIC, identifier="training", dataDir=exportPath)
+    exp.setData(export.DESCRIPTION, f"Model trained (T)/ loaded (F): {args.train}; CheckpointsPath: {checkpointsPath}; ModelsPath: {modelsPath}")
+    exp.setData(export.GENERICDATA, {"model":model})
+    exp.doExport()
