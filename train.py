1ebdda707d5ed5b45c4f0699db057fa8ed0cb0c2
stroblme <melvin.strobl@web.de>
2021-09-14 12:42:38 +0200

    fixed port var and history accuracy key

--- train.py
+++ train.py
@@ -29,9 +29,9 @@ samplingRate = 16000
 batchSize = 16
 kernelSize = 2
 epochs = 30
-port = 1050
-# PoolSize = int(multiprocessing.cpu_count()*0.3) #be gentle..
-PoolSize = 3 #be gentle..
+portion = 100
+PoolSize = int(multiprocessing.cpu_count()*0.3) #be gentle..
+# PoolSize = 3 #be gentle..
 
 if __name__ == '__main__':
     from stqft.frontend import export
@@ -40,7 +40,7 @@ if __name__ == '__main__':
 
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--waveform", default = True, action='store_true', help = "Generate Waveforms")
+    parser.add_argument("--waveform", default = True, help = "Generate Waveforms")
     parser.add_argument("--quantum", default = True, action='store_true', help = "Generate Quantum Data")
     parser.add_argument("--train", default = True, action='store_true', help = "Fit the model")
     args = parser.parse_args()
@@ -68,8 +68,8 @@ if __name__ == '__main__':
     from generateFeatures import gen_features, gen_quantum, reportSettings
     from qcnn.small_qsr import labels
     
-    if args.waveform:
-        x_train, x_valid, y_train, y_valid = gen_features(labels, datasetPath, featurePath, PoolSize, waveformPath=waveformPath, port=port)
+    if int(args.waveform):
+        x_train, x_valid, y_train, y_valid = gen_features(labels, datasetPath, featurePath, PoolSize, waveformPath=waveformPath, portion=portion)
     else:
         print("Loading from disk...")
         x_train = np.load(f"{featurePath}/x_train_speech.npy")
@@ -78,7 +78,7 @@ if __name__ == '__main__':
         y_valid = np.load(f"{featurePath}/y_valid_speech.npy")
 
     exp = export(topic=TOPIC, identifier="waveformData", dataDir=exportPath)
-    exp.setData(export.DESCRIPTION, f"Waveforms generated (T)/ loaded (F): {args.waveform}; Labels used: {labels}; FeaturePath: {featurePath}; PoolSize: {PoolSize}; WaveformPath: {waveformPath}; Portioning: {port}, SamplingRate: {samplingRate}, {reportSettings()}")
+    exp.setData(export.DESCRIPTION, f"Waveforms generated (T)/ loaded (F): {args.waveform}; Labels used: {labels}; FeaturePath: {featurePath}; PoolSize: {PoolSize}; WaveformPath: {waveformPath}; Portioning: {portion}, SamplingRate: {samplingRate}, {reportSettings()}")
     exp.setData(export.GENERICDATA, {"x_train":x_train, "x_valid":x_valid, "y_train":y_train, "y_valid":y_valid})
     exp.doExport()
 
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     from fitModel import fit_model
 
     if args.train:
-        ## For Quanv Exp.
+        # pass quanv data for training and validation
         model, history = fit_model(q_train, y_train, q_valid, y_valid, checkpointsPath)
 
         data_ix = time.strftime("%Y%m%d_%H%M")
@@ -114,5 +114,5 @@ if __name__ == '__main__':
 
     exp = export(topic=TOPIC, identifier="model", dataDir=exportPath)
     exp.setData(export.DESCRIPTION, f"Model trained (T)/ loaded (F): {args.train}; CheckpointsPath: {checkpointsPath}; ModelsPath: {modelsPath}")
-    exp.setData(export.GENERICDATA, {"history_acc":history.history['acc'], "history_val_acc":history.history['val_acc'], "history_loss":history.history['loss'], "history_val_loss":history.history['val_loss']})
+    exp.setData(export.GENERICDATA, {"history_acc":history.history['accuracy'], "history_val_acc":history.history['val_accuracy'], "history_loss":history.history['loss'], "history_val_loss":history.history['val_loss']})
     exp.doExport()
