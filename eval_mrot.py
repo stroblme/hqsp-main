57b0f3d86ba87018ed05527ef88273f6657bcf2e
stroblme <melvin.strobl@web.de>
2021-10-30 11:10:26 +0200

    direct include of params

--- eval_mrot.py
+++ eval_mrot.py
@@ -13,8 +13,31 @@ speechFile = '../dataset/left/cb8f8307_nohash_7.wav'
 if __name__ == '__main__':
     # from stqft.tests import *
     from stqft.qft import loadBackend, loadNoiseModel, setupMeasurementFitter
-    from generateFeatures import gen_mel, gen_quantum, nQubits, transpOptLvl, numOfShots, numOfRuns, suppressPrint, backend, simulation, signalThreshold, useNoiseModel, noiseMitigationOpt
+    # from generateFeatures import gen_mel, gen_quantum, nQubits, transpOptLvl, numOfShots, numOfRuns, suppressPrint, backend, simulation, signalThreshold, useNoiseModel, noiseMitigationOpt
     from stqft.frontend import frontend, export
+    nQubits=10
+    samplingRate=16000    #careful: this may be modified when calling gen_features
+    numOfShots=4096
+    signalThreshold=0.06 #optimized according to thesis
+    minRotation=0.2 #PI/2**(nQubits-4)
+    overlapFactor=0.875
+    windowLength = 2**nQubits
+    windowType='blackman'
+    suppressPrint=True
+    useNoiseModel=True
+    backend="ibmq_guadalupe" #ibmq_guadalupe, ibmq_melbourne (noisier)
+    noiseMitigationOpt=1
+    numOfRuns=1
+    simulation=True
+    transpileOnce=True
+    transpOptLvl=1
+    fixZeroSignal=False
+    scale='mel'
+    normalize=True
+    nMels=60
+    fmin=40.0
+    enableQuanv=True
+
     TOPIC = "speech_sim_n_mrot"
     export.checkWorkingTree("/media/veracrypt1/QuantumMachineLearningDevelopment/main/stqft/data")
     fri = frontend()
