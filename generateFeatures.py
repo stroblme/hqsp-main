d199ed688c35c5a495582992d64fa480f50e5de9
stroblme <melvin.strobl@web.de>
2022-06-26 22:42:27 +0200

    dummy qft callback

--- generateFeatures.py
+++ generateFeatures.py
@@ -51,6 +51,8 @@ nMels=60
 fmin=40.0
 enableQuanv=True
 
+def gen_callback(inputs):
+    pass
 
 def reportSettings():
     return f"numOfShots:{numOfShots}; signalFilter:{signalThreshold}; minRotation:{minRotation}; nSamplesWindow:{windowLength}; overlapFactor:{overlapFactor}; windowType:{windowType}; scale:{scale}; normalize:{normalize}; nMels:{nMels}; fmin:{fmin}"
@@ -90,9 +92,68 @@ def gen_mel(audioFile:str, backendInstance=backend, noiseModel=None, filterResul
         stqft.show(y_hat_stqft_p, f_p, t_p, title=f"STQFT")
     return y_hat_stqft_p
 
+def gen_wave(audioFile:str):
+    print(f"Processing {audioFile}")
+
+    y = signal(samplingRate=samplingRate, signalType='file', path=audioFile)
+    
+    return y.y
+
+def poolProcess_wave(portDatsetLabelFiles):
+
+    wave = gen_wave(portDatsetLabelFiles)
+    return wave
+
 def poolProcess(labelFileAndBackendInstance:list):
     wave = gen_mel(*labelFileAndBackendInstance)
     return np.expand_dims(wave[:,1:], axis=2)
+
+def gen_wave_features(labels:list, train_audio_path:str, outputPath:str, PoolSize:int, waveformPath:str=None, portion:int=1, split:bool=True):
+    all_wave = list()
+    all_labels = list()
+
+    for i, label in enumerate(labels):    #iterate over labels, so we don't run into concurrency issues with the mapping
+        print(f"\n---------[Label {i}/{len(labels)}]---------\n")
+        temp_waves = list()
+        
+        datasetLabelFiles = glob.glob(f"{train_audio_path}/{label}/*.wav")  #gather all label specific sample files
+
+        # TODO: maybe change that to "random"?
+        portDatsetLabelFiles = datasetLabelFiles[0::portion]   #get only a portion of those files
+        # ^ (validated) ^
+        print(f"\nUsing {len(portDatsetLabelFiles)} out of {len(datasetLabelFiles)} files for label '{label}'\n")
+
+        with Pool(PoolSize) as p:
+            temp_waves = p.map(poolProcess, portDatsetLabelFiles)   #mapping samples to processes and output back to waveform array
+        # ^ (validated) ^ When running "single threaded" in the multiprocessing.dummy module with PoolSize=1
+            # ^ (validated) ^ When running in standard multiprocessing module with PoolSize=3
+
+
+        #appending waves and labels at the END of both arrays 
+        all_wave = all_wave + temp_waves.copy() #copy to break the reference here
+        # ^ (validated) ^
+        all_labels = all_labels + [label]*len(portDatsetLabelFiles) #extend the array by the label n times
+        # ^ (validated) ^
+
+        print(f"\n Generated {len(temp_waves)} waves. In total {len(all_wave)} waves and {len(all_labels)} labels\n")
+
+    tid = time.time()
+    print(f"Finished generating waveforms at {tid}")
+
+    # if waveformPath != None:
+    #     with open(f"{waveformPath}/waveforms.pckl", 'wb') as fid:
+    #         pickle.dump(all_wave, fid, pickle.HIGHEST_PROTOCOL)
+    #     with open(f"{waveformPath}/labels.pckl", 'wb') as fid:
+    #         pickle.dump(all_labels, fid, pickle.HIGHEST_PROTOCOL)
+            
+    #     print(f"Finished dumping cache")
+    print(f"Starting Feature export")
+
+    #dirty decision, but usefull when called from test.py (where we don't need to split)
+    if split:
+        return gen_train_from_wave(all_wave=all_wave, all_label=all_labels, output=outputPath)
+    else:
+        return gen_train_from_wave_no_split(all_wave=all_wave, all_label=all_labels, output=outputPath)
 
 def gen_features(labels:list, train_audio_path:str, outputPath:str, PoolSize:int, waveformPath:str=None, portion:int=1, split:bool=True):
     all_wave = list()
